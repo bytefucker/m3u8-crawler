a8eaf3e981f46b9253e4ca76159840696fff1c9c
bytefucker <723861898@qq.com>
2023-04-03 22:47:12 +0800

    deom

--- app.py
+++ app.py
@@ -1,25 +1,42 @@
 import re
+import requests
+import os
+import json
 
 
-regex = r"(http|https):\/\/[\S]*\.m3u8"
-test_str = """
-http://43rfvdnfkdjkfgd43434.m3u8fsjgokfg;lf
-gfhbhtgdhb
-http://43rfvdnfkdjkfgd43434.m3u8
-frgfrghttp://43rfvdnfkdjkfgd43434.m3u8
-"""
+title_regex = r"<title>([\S\s]*)</title>"
+m3u8_regex = r"(http|https):\/\/[\S]*\.m3u8"
 
 
 def extract_m3u8(txt: str):
-    url = []
-    matches = re.finditer(regex, test_str, re.MULTILINE)
-    for matchNum, match in enumerate(matches, start=1):
-        print("Match {matchNum} was found at {start}-{end}: {match}"
-              .format(matchNum=matchNum, start=match.start(), end=match.end(), match=match.group()))
-        url.append(match.group())
-    return url
+    title = ''
+    urls = []
+    title_match = re.finditer(title_regex, txt, re.MULTILINE)
+    for _, match in enumerate(title_match, start=0):
+        title = match.group().replace('<title>', '').replace('</title>', "")
+
+    m3u8_matches = re.finditer(m3u8_regex, txt, re.MULTILINE)
+    for _, match in enumerate(m3u8_matches, start=1):
+        urls.append(match.group())
+    return title, urls
+
+
+def web_page(url: str):
+    resp = requests.get(url=url)
+    if resp.status_code != 200:
+        pass
+    data = extract_m3u8(resp.text)
+    dir = f'data/{data[0]}'
+    if os.path.exists(dir) is False:
+        os.mkdir(dir)
+    with open(f'{dir}/meta.json', 'w', encoding='utf-8') as f:
+        f.writelines(json.dumps(data, ensure_ascii=False))
+
+
+def download(url):
+    pass
 
 
 if __name__ == "__main__":
-    urls = extract_m3u8(test_str)
-    print(urls)
+    for i in range(0, 2000):
+        web_page(f"https://vip.aqdz198.com/videos/play/{25399-i}")
